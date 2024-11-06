28d8e2d5318cff302ef361adeb829289aa6e105f
DilyaShakirova <shakirovadilya@yandex.ru>
2024-11-06 23:21:56 +0300

    add csv file

--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -2,14 +2,16 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
-    })
+    @CsvFileSource(files ="src/test/resources/freelance.csv")
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
     public void testRest(int expected, int income, int expenses, int threshold) {
         RestService service = new RestService();
         int restMonths = service.calculate(income, expenses, threshold);
